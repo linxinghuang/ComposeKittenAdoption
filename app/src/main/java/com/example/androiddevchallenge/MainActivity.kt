3306fa92ecdb83fe9d23d20f93aabe58792fe3bb
linxinghuang <fsjdhuang@163.com>
2021-03-03 14:44:48 +0800

    commit

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -16,14 +16,30 @@
 package com.example.androiddevchallenge
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.androiddevchallenge.data.Cat
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import com.example.androiddevchallenge.viewmodel.MainViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,23 +55,107 @@ class MainActivity : AppCompatActivity() {
 // Start building your app here!
 @Composable
 fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+    val snackBarHostState = SnackbarHostState()
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Kitten Adoption")
+                }
+            )
+        },
+        snackbarHost = {
+            SnackbarHost(snackBarHostState)
+        }) {
+
+        val viewModel = MainViewModel()
+        CatList(viewModel)
+        viewModel.currentCat?.let {
+            CatDetails(it) { cat ->
+                coroutineScope.launch {
+                    snackBarHostState.showSnackbar("You have adopted ${cat.name}")
+                }
+            }
+        }
     }
 }
 
-@Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
-fun LightPreview() {
-    MyTheme {
-        MyApp()
+fun CatList(viewModel: MainViewModel) {
+    LazyColumn(
+        contentPadding = PaddingValues(5.dp),
+        verticalArrangement = Arrangement.spacedBy(5.dp)
+    ) {
+        items(items = viewModel.cats) { cat ->
+            Card(
+                Modifier
+                    .fillMaxWidth()
+                    .clickable {
+                        if (viewModel.currentCat == null) {
+                            viewModel.showDetails(cat)
+                        }
+                    }) {
+                Box {
+                    Image(
+                        painterResource(cat.photo),
+                        contentDescription = "Picture of cat: ${cat.name}",
+                        Modifier
+                            .fillMaxWidth()
+                            .height(240.dp),
+                        contentScale = ContentScale.Crop
+                    )
+
+
+                    Column(Modifier.padding(paddingValues = PaddingValues(start = 5.dp))) {
+                        Text(cat.name, color = Color.White, style = MaterialTheme.typography.h4)
+                        Text(cat.age, color = Color.White, style = MaterialTheme.typography.body1)
+                        Text(
+                            cat.describe,
+                            color = Color.White,
+                            style = MaterialTheme.typography.body2
+                        )
+                    }
+                }
+            }
+        }
     }
 }
 
-@Preview("Dark Theme", widthDp = 360, heightDp = 640)
 @Composable
-fun DarkPreview() {
-    MyTheme(darkTheme = true) {
-        MyApp()
+fun CatDetails(cat: Cat, onClick: (Cat) -> Unit = {}) {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background)
+    ) {
+        Box {
+            Image(
+                painterResource(cat.photo),
+                "Photo: ${cat.name}",
+                Modifier.aspectRatio(1f),
+                contentScale = ContentScale.Crop
+            )
+            Button(
+                { onClick(cat) },
+                Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(16.dp)
+            ) {
+                Text(text = "Adopt")
+            }
+        }
+        Column(Modifier.padding(16.dp, 8.dp)) {
+            Text("Name: ${cat.name}", style = MaterialTheme.typography.h4)
+            ProvideTextStyle(MaterialTheme.typography.h5) {
+                Text("Gender: ${cat.gender}")
+                Text("Age: ${cat.age}")
+                Text("Gender: ${cat.gender}")
+                Text("Weight: ${cat.weight}")
+                Text("Describe: ${cat.describe}")
+            }
+        }
     }
 }
+
+
---
+++ app/src/main/java/com/example/androiddevchallenge/viewmodel/MainViewModel.kt
@@ -0,0 +1,100 @@
+package com.example.androiddevchallenge.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.androiddevchallenge.R
+import com.example.androiddevchallenge.data.Cat
+import androidx.compose.runtime.setValue
+
+class MainViewModel : ViewModel() {
+    val cats by mutableStateOf(
+        listOf(
+            Cat(
+                "Anan",
+                "Male",
+                "Two month old",
+                "4kg",
+                "He is so Beautiful and long hair",
+                R.drawable.img_cat_anan
+            ),
+            Cat(
+                "Bily",
+                "Femal",
+                "Six month",
+                "1.75kg",
+                "She is white cat",
+                R.drawable.img_cat_bily
+            ),
+            Cat(
+                "Cloudy",
+                "Male",
+                "Eight month",
+                "2kg",
+                "He is cute",
+                R.drawable.img_cat_cloudy
+            ),
+            Cat(
+                "Kitora",
+                "Male",
+                "One year three month",
+                "3kg",
+                "He has beautiful eyes ",
+                R.drawable.img_cat_kitora
+            ),
+            Cat(
+                "Kiki",
+                "Male",
+                "One month",
+                "0.65kg",
+                "He is so small and cute",
+                R.drawable.img_cat_kiki
+            ),
+            Cat(
+                "Lisa",
+                "Female",
+                "One year three month",
+                "2.65kg",
+                "Her body is long",
+                R.drawable.img_cat_lisa
+            ),
+            Cat(
+                "Pina",
+                "Female",
+                "One year three month",
+                "2.65kg",
+                "She body is long",
+                R.drawable.img_cat_pina
+            ),
+            Cat(
+                "Soda",
+                "Male",
+                "One month",
+                "0.43kg",
+                "His ear is cute",
+                R.drawable.img_cat_soda
+            ),
+            Cat(
+                "Tatami",
+                "Male",
+                "Four month",
+                "1.56kg",
+                "He like sleeping",
+                R.drawable.img_cat_tatami
+            ),
+
+            )
+    )
+
+    var currentCat: Cat? by mutableStateOf(cats[0])
+
+
+    fun showDetails(cat: Cat) {
+        this.currentCat = cat
+    }
+
+    fun closeDetails() {
+        this.currentCat = null
+    }
+
+}
